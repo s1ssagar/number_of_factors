ccef6aeebbda97e805930457260473bfe664b271
Sagar <s1ssagar.s1ssingh@gmail.com>
2017-09-28 17:36:23 +0530

    Find factors of a number

---
+++ README.md
@@ -0,0 +1,3 @@
+Program to find factors of a given number. Using number systems logic. First we find the prime factors of a number and then find the count of the prime factors, example say number is 30, it has 3 prime factors 2, 3, 5 all are present 1 times so the formula to get all factors are (1+1)*(1+1)*(1+1) i.e 8. Lets look at another example say the number is 100, we have two prime factors 2 and 5 both are present two times so using the formula we will have (2+1)*(2+1) i.e equal to 9.
+
+To use the progarm type python <filename> and press enter.
---
+++ num_factors.py
@@ -0,0 +1,39 @@
+import math
+
+def factors(num):
+    prime_fac = []
+    num_of_fac = []
+    # A number has factors only upto its Sqrt
+    max_fac = int(math.sqrt(num)+1)
+    for x in range(1, max_fac+1):
+        count = 0
+        if num % x == 0:
+            for y in range(1, x):
+                if x % y == 0:
+                    count += 1
+                    if count > 1:
+                        break
+            if count == 1:
+                prime_fac.append(x)
+    for factor in prime_fac:
+        count = 0
+        while num % factor == 0:
+            count += 1
+            num = num / factor
+        num_of_fac.append(count)
+    # print prime_fac, num_of_fac
+    if prime_fac == [] and num == 0:
+        print "zero is the only integer that has infinitely many factors, and zero is the only integer that has zero for a factor."
+    elif prime_fac == [] and num != 1:
+        print "Number of factors are:", 2
+    else:
+        product = 1
+        for prod in num_of_fac:
+            product *= (prod+1)
+        print "Number of factors are:", product
+
+def main():
+    factors(abs(input("Enter the number ")))
+
+if __name__ == "__main__":
+    main()
